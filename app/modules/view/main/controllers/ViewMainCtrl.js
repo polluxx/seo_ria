1164a00eb24dd5c5a46452162733dc165adc6439
polluxx <yuriy.kosakivsky@gmail.com>
2015-02-24 10:24:21 +0200

    add fixes

--- app/modules/view/main/controllers/ViewMainCtrl.js
+++ app/modules/view/main/controllers/ViewMainCtrl.js
@@ -98,11 +98,11 @@ define([
 
             if($scope.varsLoading) {
                 setInterval(function() {
-                    return $scope.checkForRewrites(callback);
+                    $scope.checkForRewrites(callback);
                 }, 200);
             } else if(Object.keys($scope.varsRewrites).length > 0) {
                 console.log($scope.varsRewrites)
-                return callback($scope.varsRewrites);
+                callback($scope.varsRewrites);
             } else {
                 $scope.getVarsNames(function () {
                     callback($scope.varsRewrites);
@@ -115,7 +115,7 @@ define([
             //console.log(items);
 
             if(Object.keys($scope.varsRewrites).length > 0) {
-                return callback($scope.varsRewrites);
+                callback($scope.varsRewrites);
             } else {
 
                 return $scope.checkForRewrites(callback);
