b0c07480af7a5fdd0d3352176622dfa672f2f2e1
polluxx <yuriy.kosakivsky@gmail.com>
2015-03-26 11:54:50 +0200

    add CM

--- cmApp/base/home/directives/BaseDirective.js
+++ cmApp/base/home/directives/BaseDirective.js
@@ -681,7 +681,8 @@ define(['base/home/module'], function (module) {
                         for(var substyle in substyles) {
                             if(typeof substyles[substyle] === "function") continue;
 
-                            string += "informer "+ substyle+":"+substyles[substyle]+";\n";
+
+                            string += "infoblock "+ substyle+":"+substyles[substyle]+";\n";
                         }
                         string += "} \n";
 
